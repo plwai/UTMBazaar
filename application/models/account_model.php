e71bf5e7f36f303ed63b7526e0352bb850944b24
plwai <pailee.wai@gmail.com>
2015-09-23 00:50:47 +0900

    Account_model is added with get_user method

---
+++ application/models/account_model.php
@@ -0,0 +1,35 @@
+<?php
+
+class Account_model extends CI_Model{
+
+  // Search for the user and return its data
+  // Return true or false if only username passed
+  public function get_user($username, $password = ''){
+    $this->db->where('username', $username);
+    $query = $this->db->get('utm_users');
+
+    // Check the user whether exist in the database
+    if($query->num_rows == 1 && $password == ''){
+      $data = array(
+        'isSuccess' => true
+      );
+    }
+    else if($query->num_rows == 1){
+      $row = $query->result_array();
+      $data = array(
+        'username'     => $row[0]['id'],
+				'password'     => $row[0]['password'],
+				'enc_password' => $row[0]['enc_password'],
+				'last_login'   => $row[0]['last_login'],
+				'isSuccess'    => true
+      );
+    }
+    else{
+      $data = array(
+        'isSuccess' => false
+      );
+    }
+
+    return $data;
+  }
+}
