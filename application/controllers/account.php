bfd53cdba4928597bc5b207d788d9092e3bfe138
plwai <pailee.wai@gmail.com>
2015-09-23 00:49:00 +0900

    account controller with reset password method

---
+++ application/controllers/account.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Account controller handle account function.
+class Account extends CI_Controller{
+
+  // Account controller constructor
+  public function index(){
+
+    // check whether user login
+    /*if($this->session->userdata('is_logged_in')){
+			redirect('home');
+		}
+		else{
+			$this->load->view('login_view');
+		}*/
+  }
+
+  /* Reset password function that will generate a link  *
+   * to user's email for resetting password             */
+  public function reset_password($type = ''){
+
+    // Handle user data(username or email) sent by user
+    if($this->input->server('REQUEST_METHOD') === 'POST'){
+      $this->form_validation->set_rules('email', 'Email', 'required');
+
+      if($this->form_validation->run()){
+
+        // Get data from reset_view form
+        $username  = $this->input->post('email');
+
+        $this->load->model('account_model');
+				$userdata = $this->account_model->get_user();
+
+        if($userdata['isSuccess']){
+          $data['email'] = $username;
+          $this->load->view('resetlink_view', $data);
+        }
+      }
+    }
+    else{
+      $this->load->view('reset_view');
+    }
+  }
+
+  // Resend email to user
+  private function resend_email($content){
+
+  }
+}
