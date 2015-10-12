27d72725d520e1cfa16c78e61db3e2419e28f72d
yasminzahir <nimsay94@gmail.com>
2015-10-12 19:59:02 +0800

    .

--- application/views/update_view.php
+++ application/views/update_view.php
@@ -1,37 +1,37 @@
 <html>
 	<head>
-		<title>Update Data In Database Using CodeIgniter</title>
+		<title>UTM BazaaR</title>
 			<link href='http://fonts.googleapis.com/css?family=Marcellus' rel='stylesheet' type='text/css'>
 			<link rel="stylesheet" type="text/css" href="<?php echo base_url(). "css/update.css" ?>">
 	</head>
 		<body>
 			<div id="container">
 				<div id="wrapper">
-					<h1>Update Data In Database Using CodeIgniter </h1><hr/>
+					<h1>UTM BazaaR</h1><hr/>
 						<div id="menu">
 							<p>Click On Menu</p>
-							<!-- Fetching Names Of All Students From Database -->
+							<!-- Fetching Names Of All Users From Database -->
 							<ol>
-					<?php foreach ($students as $student): ?>
-					<li><a href="<?php echo base_url() . "index.php/update_ctrl/show_student_id/" . $student->student_id; ?>"><?php echo $student->student_name; ?></a></li>
+					<?php foreach ($utm_users as $user): ?>
+					<li><a href="<?php echo base_url() . "index.php/update_ctrl/show_user_id/" . $user->pkid; ?>"><?php echo $user->name; ?></a></li>
 					<?php endforeach; ?>
 							</ol>
 						</div>
 				<div id="detail">
-				<!-- Fetching All Details of Selected Student From Database And Showing In a Form -->
-					<?php foreach ($single_student as $student): ?>
+				<!-- Fetching All Details of Selected User From Database And Showing In a Form -->
+					<?php foreach ($single_user as $user): ?>
 					<p>Edit Detail & Click Update Button</p>
-					<form method="post" action="<?php echo base_url() . "index.php/update_ctrl/update_student_id1"?>">
+					<form method="post" action="<?php echo base_url() . "index.php/update_ctrl/update_user_id1"?>">
 						<label id="hide">Id :</label>
-							<input type="text" id="hide" name="did" value="<?php echo $student->student_id; ?>">
+							<input type="text" id="hide" name="did" value="<?php echo $user->pkid; ?>">
 						
 						<label>Name :</label>
-							<input type="text" name="dname" value="<?php echo $utm_users->name; ?>">
+							<input type="text" name="name" value="<?php echo $user->name; ?>">
 						
 						<label>Email :</label>
-							<input type="text" name="demail" value="<?php echo $utm_users->email; ?>">
+							<input type="text" name="email" value="<?php echo $user->email; ?>">
 						
-						<input type="submit" id="submit" name="dsubmit" value="Update">
+						<input type="submit" id="submit" name="submit" value="Update">
 						
 					</form>
 				<?php endforeach; ?>
