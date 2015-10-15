9ccb34ca618aa1ab9b76829675e08370dccd535e
weequan93 <weequan@live.com>
2015-10-16 01:29:44 +0800

    session added

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -11,9 +11,14 @@ class Products extends CI_Controller{
     }
 
     public function view_products($owner_id=null){
+        if($this->session->userdata('is_logged_in')){
+            $username = $this->session->userdata('username');
+            $data['username']   = $username;
+
+        }
         $_data['query'] = $this->Product_model->view_products($owner_id);
         $data['title'] = 'showproducts';
-        $data['display'] = 'display:none;';
+        $data['display'] = '';
         $this->load->view('template/header.php', $data);
         $this->load->view('views_products_view', $_data);
     }
