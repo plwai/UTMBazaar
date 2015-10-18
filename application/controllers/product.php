5a478ddb95e748ce039874a4150072e59a9d2597
yasminzahir <nimsay94@gmail.com>
2015-10-18 18:40:10 +0800

    edited

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -4,6 +4,7 @@ class Product extends CI_Controller {
 
 	public function index($index = 0)
 	{
+		$this->load->model ('MProduct');
 		$data['listProduct'] = $this->mproduct->findAll();
 		$this->load->view('template/header.php', $data);
 		$this->load->view('index', $data);
